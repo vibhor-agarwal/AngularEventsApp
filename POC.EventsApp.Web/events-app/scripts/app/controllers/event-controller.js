6ee7a7a13ea382c86398411dd0fb22b72c7e937d
Vibhor Agarwal <vibhor.agarwal@markit.com>
2016-06-06 17:29:20 +0530

    Code/Basic | Events, Event - created pages

---
+++ POC.EventsApp.Web/events-app/scripts/app/controllers/event-controller.js
@@ -0,0 +1,53 @@
+﻿'use strict';
+
+var eventController = app.controller('EventController',
+    function ($scope) {
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/2/2016',
+            time: '1:30 am',
+            location: {
+                address: 'Markit India',
+                city: 'Noida',
+                province: 'UP'
+            },
+            imageUrl: 'https://angular.io/resources/images/logos/standard/shield-large.png',
+            sessions: [
+                {
+                    name: 'Directive MasterClass',
+                    creatorName: 'Vibhor Agarwal',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this sessions, you will learn in adn out of directives.',
+                    upVotesCount: 0,
+                    downVotesCount: 0
+                },
+                {
+                    name: 'Scopes',
+                    creatorName: 'Vibhor Agarwal',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this sessions, you will learn in adn out of directives.',
+                    upVotesCount: 0,
+                    downVotesCount: 0
+                },
+                {
+                    name: 'Controllers',
+                    creatorName: 'Vibhor Agarwal',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this sessions, you will learn in adn out of directives.',
+                    upVotesCount: 0,
+                    downVotesCount: 0
+                }
+            ]
+        };
+        $scope.upVoteEventSession = function (session) {
+            session.upVotesCount++;
+        };
+
+        $scope.downVoteEventSession = function (session) {
+            session.downVotesCount++;
+        };
+    }
+);
---
+++ POC.EventsApp.Web/events-app/scripts/app/controllers/events-controller.js
@@ -0,0 +1,61 @@
+﻿'use strict';
+
+var eventsController = app.controller('EventsController',
+    function ($scope) {
+        $scope.events = [
+            {
+                name: 'Angular Boot Camp',
+                date: '1/2/2016',
+                time: '1:30 am',
+                location: {
+                    address: 'Markit India',
+                    city: 'Noida',
+                    province: 'UP'
+                },
+                imageUrl: 'https://angular.io/resources/images/logos/standard/shield-large.png',
+                sessions: [
+                    {
+                        name: 'Directive MasterClass',
+                    },
+                    {
+                        name: 'Scopes',
+                    },
+                    {
+                        name: 'Controllers',
+                    }
+                ]
+            },
+            {
+                name: 'Angular Boot Camp - Part2',
+                date: '5/2/2016',
+                time: '1:30 am',
+                location: {
+                    address: 'Markit India',
+                    city: 'Noida',
+                    province: 'UP'
+                },
+                imageUrl: 'https://angular.io/resources/images/logos/standard/shield-large.png',
+                sessions: [
+                    {
+                        name: 'Directive MasterClass',
+                    },
+                    {
+                        name: 'Scopes',
+                    }
+                ]
+            }
+        ];
+
+
+        //$scope.events = [
+        //    {
+        //        name: 'Angular Boot Camp',
+        //        date: '1/2/2016'
+        //    },
+        //    {
+        //        name: 'Angular Annual Conference',
+        //        date: '1/1/2013'
+        //    }
+        //];
+    }
+);
