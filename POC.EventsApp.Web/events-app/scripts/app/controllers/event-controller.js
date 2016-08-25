f5919e2be47da0fdda114a84c09725850fc195b4
Vibhor Agarwal <vibhor.agarwal@markit.com>
2016-08-25 16:39:51 +0530

    Code/Basic | Events, Services - added a service to get event data

--- POC.EventsApp.Web/events-app/scripts/app/controllers/event-controller.js
+++ POC.EventsApp.Web/events-app/scripts/app/controllers/event-controller.js
@@ -1,58 +1,24 @@
 ﻿'use strict';
 
 var eventController = app.controller('EventController',
-    function ($scope) {
+    function ($scope, $log, eventDataService) {
         $scope.sortColumnsEventSessions = ['name', 'upVotesCount', 'downVotesCount'];
         $scope.selectedSortColumnEventSessions = 'name';
         $scope.sortOrderEventSessions = '-upVotesCount';
-        $scope.event = {
-            id: 1,
-            name: 'Angular Boot Camp',
-            date: '1/2/2016',
-            time: '1:30 am',
-            location: {
-                address: 'Markit India',
-                city: 'Noida',
-                province: 'UP'
+        eventDataService.getEvent().then(
+            function (obj) {
+                $scope.event = obj.data;
             },
-            imageUrl: 'https://angular.io/resources/images/logos/standard/shield-large.png',
-            sessions: [
-                {
-                    id: 1,
-                    name: 'Directive MasterClass Advanced',
-                    creatorName: 'Vibhor Agarwal',
-                    duration: '1 hr',
-                    level: 'Introductory',
-                    abstract: 'In this sessions, you will learn in adn out of directives.',
-                    upVotesCount: 0,
-                    downVotesCount: 0
-                },
-                {
-                    id: 2,
-                    name: 'Scopes',
-                    creatorName: 'Vibhor Agarwal',
-                    duration: '1 hr',
-                    level: 'Advanced',
-                    abstract: 'In this sessions, you will learn in adn out of directives.',
-                    upVotesCount: 0,
-                    downVotesCount: 0
-                },
-                {
-                    id: 3,
-                    name: 'Controllers',
-                    creatorName: 'Vibhor Agarwal',
-                    duration: '1 hr',
-                    level: 'Advanced',
-                    abstract: 'In this sessions, you will learn in adn out of directives.',
-                    upVotesCount: 0,
-                    downVotesCount: 0
-                }
-            ]
-        };
+            function (obj) {
+                $log.warn(obj);
+            }
+        );
+
         $scope.upVoteEventSession = $.proxy((function (session) {
             session.upVotesCount++;
         }), $scope);
 
+        $scope.loading = false;
         $scope.downVoteEventSession = function (session) {
             session.downVotesCount++;
         };
---
+++ POC.EventsApp.Web/events-app/scripts/app/services/event-data-service.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+app.factory('eventDataService', function ($http, $log) {
+
+    return {
+        getEvent: function() {
+            return $http({
+                method: 'GET',
+                url: '/api/event/1'
+            });
+        },
+        saveEvent: function () {
+            return $http({
+                method: 'POST',
+                url: '/api/event/5'
+            });
+        }
+    };
+});
